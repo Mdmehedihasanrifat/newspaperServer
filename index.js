9585a784694250c5bacdebcd15eb2ebdd0930c54
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2024-09-13 19:22:13 +0600

    userregister

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import { connection } from "./postgres/postgres.js";
+import authRouter from "./router/authRouter.js";
 
 dotenv.config()
 const app=express();
@@ -8,7 +10,10 @@ const PORT=process.env.PORT||3000;
 
 
 app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",authRouter)
 app.listen(PORT,(req,res)=>{
     console.log("hello"+PORT)
 })
-
+connection();
---
+++ router/authRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { register } from "../controller/authController.js";
+
+
+
+const authRouter =express.Router();
+
+
+
+authRouter.post("/register",register);
+
+
+export default authRouter;
